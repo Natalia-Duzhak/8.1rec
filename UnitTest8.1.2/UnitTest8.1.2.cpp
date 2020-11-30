7226b1542802999858565f69ad8d25834f06dd7f
Natalia-Duzhak <72571464+Natalia-Duzhak@users.noreply.github.com>
2020-11-30 18:08:15 +0300

    Add files via upload

---
+++ UnitTest8.1.2/UnitTest8.1.2.cpp
@@ -0,0 +1,27 @@
+﻿
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Project8.1.2/Source.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest812
+{
+	TEST_CLASS(UnitTest812)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+#define _CRT_SECURE_NO_WARNINGS
+		{
+
+
+			int i = 0;
+			char str[10] = { '+','\0' };
+
+			int m;
+			m = Count(str,0);
+			Assert::AreEqual(m, 0);
+		}
+	};
+}
